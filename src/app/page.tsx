48011fd1455c3a8d64718183019882fb2afa3ea5
Saravanan <saravanan2003cr7@gmail.com>
2025-04-18 14:06:50 +0530

    final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { Posts } from "@/components/blog/Posts";
 export async function generateMetadata() {
   const title = home.title;
   const description = home.description;
-  const ogImage = `https://${baseURL}/og?title=${encodeURIComponent(title)}`;
+ 
 
   return {
     title,
@@ -21,18 +21,13 @@ export async function generateMetadata() {
       description,
       type: "website",
       url: `https://${baseURL}`,
-      images: [
-        {
-          url: ogImage,
-          alt: title,
-        },
-      ],
+      
     },
     twitter: {
       card: "summary_large_image",
       title,
       description,
-      images: [ogImage],
+      
     },
   };
 }
